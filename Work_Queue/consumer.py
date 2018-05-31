b6331b2056dd1e8c2da0d419897232372a4c5d31
zhangmingyang <991469838@qq.com>
2018-05-31 08:48:43 +0800

    如何防止Rabbit服务挂掉:队列持久化+消息持久化

--- Work_Queue/consumer.py
+++ Work_Queue/consumer.py
@@ -7,17 +7,17 @@ import time
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='queue_2')
+channel.queue_declare(queue='queue_4',durable=True)  #队列持久化.
 
 def callback(ch,method,properties,body):
     print('[x] 消费者接受到的消息是:%r'%body)
     time.sleep(len(body))
     print('[x] 处理消息完毕.')
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)   #防止消费者挂掉
 
 channel.basic_consume(consumer_callback=callback,
-                      queue='queue_2',
-                      no_ack=False)
+                      queue='queue_4',
+                      no_ack=False)    #防止消费者挂掉.
 
 print('消费者开始等待消息..')
 
--- Work_Queue/producer.py
+++ Work_Queue/producer.py
@@ -7,12 +7,14 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='queue_2')
+channel.queue_declare(queue='queue_4',durable=True)   #队列持久化
 
 message = ''.join(sys.argv[1:]) or 'Hello World!'
 channel.basic_publish(exchange='',
-                      routing_key='queue_2',
-                      body=message)
+                      routing_key='queue_4',
+                      body=message,
+                      properties=pika.BasicProperties(delivery_mode=2))  #消息持久化.
+
 
 print('生产者发送的消息是:%s...'%message)
 
